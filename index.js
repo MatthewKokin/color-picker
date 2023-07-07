d745d3eb4dbbea574604ee9bec26d491499e80a7
MatthewKokin <135328101+MatthewKokin@users.noreply.github.com>
2023-07-07 13:32:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 ({plugins: ['jsdom-quokka-plugin']})
 
-
 const inputColour = document.getElementById("input");
 const dropdown = document.getElementById("dropdown");
 
@@ -8,10 +7,29 @@ document.getElementById("btn").addEventListener("click", function() {
     fetch(`https://www.thecolorapi.com/scheme?hex=${inputColour.value.slice(1)}&mode=${dropdown.value}`)
         .then(res => res.json())
         .then(data => {
-            data.colors.forEach((color, i, array) => {
-                document.getElementsByClassName(`color${i + 1}`)[0].style.background = color.hex.value;
-                document.getElementsByClassName(`text${i + 1}`)[0].style.content = color.hex.value;
-                console.log(array)
+            const batchStyles = [];
+
+            data.colors.forEach((color, i) => {
+                const colorElement = document.getElementsByClassName(`color${i + 1}`)[0];
+                const textElement = document.getElementsByClassName(`text${i + 1}`)[0];
+
+                batchStyles.push({
+                    colorElement,
+                    textElement,
+                    backgroundColor: color.hex.value,
+                    content: color.hex.value
+                });
             });
+
+            applyBatchStyles(batchStyles);
         });
 });
+
+function applyBatchStyles(styles) {
+    requestAnimationFrame(() => {
+        styles.forEach(style => {
+            style.colorElement.style.background = style.backgroundColor;
+            style.textElement.style.content = style.content;
+        });
+    });
+}
